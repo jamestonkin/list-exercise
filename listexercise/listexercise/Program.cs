c04b5271eaaa965b8e1a32533f44f647eda90c8e
James W. Tonkin <btonkin71@comcast.net>
2017-07-18 18:14:48 -0500

    Exercise completed

---
+++ listexercise/listexercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> lastTwoPlanets = new List<string>() { "Uranus", "Neptune" };
+
+            planetList.AddRange(lastTwoPlanets);
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = new List<string>(planetList.GetRange(0, 4));
+
+            planetList.Remove("Pluto");
+
+            foreach(string rocky in rockyPlanets)
+            {
+                Console.WriteLine(rocky);
+            }
+
+            foreach(string planets in planetList)
+            {
+                Console.WriteLine(planets);
+            }
+            Console.Read();
+        }
+    }
+}
